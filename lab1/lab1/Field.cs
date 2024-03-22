3dd370fa9218874a03e58f4fb5d3f3ef865ecfed
darararrik <imyaminovdamir10@gmail.com>
2024-03-22 21:34:27 +0600

    Сделал 1 задачу 1.4

---
+++ lab1/lab1/Field.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Field
+    {
+        private char[,] field;
+        private int n;
+        private int m;
+        public Field(int n,int m)
+        {
+            this.n = n;
+            this.m = m;
+            field = new char[n,m];
+        }
+        public void FillField()
+        {
+            for (int i = 0; i < n; i++) 
+            {
+                string row = Console.ReadLine();
+ 
+                for (int j = 0; j < m; j++)
+                    if (row[i] == '*' || row[i] == '.')
+                        field[i, j] = row[j];
+                    else
+                        Console.WriteLine("Нужно вводить . или *\nВ строке в которой был введен не верный символ, вся строка была заменена на ."); break;
+
+
+
+
+
+
+
+
+            }
+        }
+        public void FinalField(int number)
+        {
+            StringBuilder res = new StringBuilder();
+            res.AppendLine($"Field #{number}");
+            for (int i = 0; i< n; i++)
+            {
+
+                for(int j = 0;j< m; j++)
+                {
+                    if (field[i,j] == '*')
+                        res.Append('*');
+                    else res.Append(CountField(i,j));
+                }
+                res.AppendLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine(res);
+        }
+        private int CountField(int x, int y)
+        {
+            int count = 0;
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int j = y - 1; j <= y + 1; j++)
+                {
+                    if (i >= 0 && i < n && j >= 0 && j < m && field[i, j] == '*')
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using lab1;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int fieldNumber = 1;
+
+
+        while (true)
+        {
+            Console.WriteLine("Введите размер поля через пробел (N M)");
+            Console.WriteLine("Если ввести 0 0, программа завершится");
+            string[] sizes = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(sizes[0]);
+            int m = Convert.ToInt32(sizes[1]); 
+            if (n < 0 || m < 0) 
+            {
+                Console.WriteLine("Введены не правильные значения.");
+                continue;
+            }
+            if (n == 0 && m == 0)
+                break;
+
+            Field field = new Field(n, m);
+            field.FillField();
+            field.FinalField(fieldNumber);
+           
+            fieldNumber++;
+        }
+
+
+    }
+}
