0c8fd59ebce78973cfda9196fe5ac70e2dc2e486
darararrik <imyaminovdamir10@gmail.com>
2024-05-06 21:20:20 +0600

    Upd

--- lab5/lab4_2/Program.cs
+++ lab5/lab4_2/Program.cs
@@ -1,132 +1,251 @@
-﻿using Lucene.Net.Util;
-using System.Runtime.Intrinsics.Arm;
-using static Program.Intersections;
-internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+class Intersections
 {
-    /*Алгоритм Дейкстры: 
-     * Максимальное расстояние: Для каждого перекрестка найдите максимальное расстояние от перекрестка до ближайшего депо.
-     * Выбор оптимального местоположения: Выберите перекресток с минимальным максимальным расстоянием из шага 2. 
-     * Это будет оптимальное местоположение для нового пожарного депо.
-     * Размещение нового депо: Разместите новое пожарное депо на выбранном перекрестке*/
-    private static void Main(string[] args)
+    public int from;
+    public int to;
+    public int distance;
+    public bool hasDepo;
+
+    public Intersections(int from, int to, int distance)
+    {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+        this.hasDepo = false;
+    }
+}
+
+class FireStationPlacement
+{
+    static void Main()
     {
         int t = int.Parse(Console.ReadLine());
         while (t-- > 0)
         {
             string[] inputs = Console.ReadLine().Split(' ');
-            int f = int.Parse(inputs[0]);// кол-во депо
-            int I = int.Parse(inputs[1]);// кол-во перекрестков
-            int OptimalDistance = 0;
+            int f = int.Parse(inputs[0]); // кол-во депо
+            int I = int.Parse(inputs[1]); // кол-во перекрестков
+
             List<Intersections> intersections = new List<Intersections>();
-           
-            int[] DepoNumber = new int[f];
-            for(int i=0;i<f;i++)
+
+            int[] depoNumber = new int[f];
+            for (int i = 0; i < f; i++)
             {
-                DepoNumber[i] = int.Parse(Console.ReadLine()) - 1;
+                depoNumber[i] = int.Parse(Console.ReadLine()) - 1;
             }
-            string[] line;
+
             for (int i = 0; i < I; i++)
             {
-                line = Console.ReadLine().Split(' ');
+                string[] line = Console.ReadLine().Split(' ');
                 intersections.Add(new Intersections(int.Parse(line[0]), int.Parse(line[1]), int.Parse(line[2])));
             }
-            for (int i = 0;i<f;i++)
-            {
-                intersections[DepoNumber[i]].hasDepo = true;
 
+            // Устанавливаем флаги депо для перекрестков
+            foreach (int depoIndex in depoNumber)
+            {
+                intersections[depoIndex].hasDepo = true;
             }
 
-                (int optimalIntersectionNumber, int optimalDistance) = Dijkstra(intersections, f, I, DepoNumber);
-            Console.WriteLine("Лучшее местоположение для депо: " + optimalIntersectionNumber);
-
-
-        }
-        Console.ReadLine();
-    }
-    private static (int IntersectionNumber, int MaxDistance) Dijkstra(List<Intersections> intersections, int f, int I, int[] DepoNumber)
-    {
-        // Создаем массив расстояний до каждого перекрестка и инициализируем его бесконечностью
-        int[] distances = new int[I];
-        for (int i = 0; i < I; i++)
-        {
-            distances[i] = int.MaxValue;
-        }
-
-        // Массив флагов, чтобы отслеживать, был ли уже посещен перекресток
-        bool[] visited = new bool[I];
-
-        // Добавляем фиктивное депо на все перекрестки, где оно не было установлено
-        foreach (int depo in DepoNumber)
-        {
-            intersections[depo].hasDepo = true;
-            distances[depo] = 0; // Расстояние от депо до самого себя равно 0
-        }
-
-        // Запускаем алгоритм Дейкстры
-        while (true)
-        {
-            int minDistance = int.MaxValue;
-            int minIndex = -1;
-
-            // Находим перекресток с минимальным расстоянием, который еще не был посещен
+            // Применяем алгоритм Флойда-Уоршелла
+            int[,] distances = new int[I, I];
             for (int i = 0; i < I; i++)
             {
-                if (!visited[i] && distances[i] < minDistance)
+                for (int j = 0; j < I; j++)
                 {
-                    minDistance = distances[i];
-                    minIndex = i;
+                    if (i == j)
+                        distances[i, j] = 0;
+                    else
+                        distances[i, j] = -1;
                 }
             }
 
-            // Если все перекрестки были посещены, выходим из цикла
-            if (minIndex == -1)
-                break;
-
-            visited[minIndex] = true;
-
-            // Обновляем расстояния до соседа текущего перекрестка
-            int neighbor = intersections[minIndex].Neighbor;
-            int distanceToNeighbor = intersections[minIndex].Distance;
-            int newDistance = distances[minIndex] + distanceToNeighbor;
-            if (newDistance < distances[neighbor])
+            for (int i = 1; i <= I; i++)
             {
-                distances[neighbor] = newDistance;
-            }
-        }
-
-        // Находим максимальное расстояние от фиктивного депо до всех перекрестков
-        int maxDistance = int.MinValue;
-        int optimalIntersectionNumber = -1; // Номер перекрестка с оптимальным расположением депо
-        for (int i = 0; i < I; i++)
-        {
-            if (!intersections[i].hasDepo) // Пропускаем уже существующие депо
-            {
-                if (distances[i] > maxDistance)
+                for (int j = 1; j <= I; j++)
                 {
-                    maxDistance = distances[i];
-                    optimalIntersectionNumber = i + 1; // Номер перекрестка на единицу больше индекса
+                    if (i != j)
+                    {
+                        foreach (Intersections intersection in intersections)
+                        {
+                            distances[intersection.from - 1, intersection.to - 1] = intersection.distance;
+                            distances[intersection.to - 1, intersection.from - 1] = intersection.distance;
+
+
+                        }
+                    }
+
                 }
             }
+            #region ShowArray
+            Console.WriteLine("    1    2    3    4    5    6");
+            Console.WriteLine("    ---------------------------");
+            for (int i = 0; i < I; i++)
+            {
+                Console.Write($"{i + 1}|");
+
+
+                for (int j = 0; j < I; j++)
+                {
+                    Console.Write($"   {distances[i, j]}");
+                }
+                Console.WriteLine($" |");
+            }
+            #endregion
+
+            for (int k = 0; k < I; k++)
+            {
+                for (int i = 0; i < I; i++)
+                {
+                    for (int j = 0; j < I; j++)
+                    {
+                        // Если существует путь от i к j через вершину k
+                        if (distances[i, k] != -1 && distances[k, j] != -1)
+                        {
+                            // Если distance[i, j] еще не установлено или новый путь короче
+                            if (distances[i, j] == -1 || distances[i, k] + distances[k, j] < distances[i, j])
+                            {
+                                distances[i, j] = distances[i, k] + distances[k, j];
+                            }
+                        }
+                    }
+                }
+            }
+
+            #region ShowArray
+            Console.WriteLine("    1    2    3    4    5    6");
+                Console.WriteLine("    ---------------------------");
+                for (int i = 0; i < I; i++)
+                {
+                    Console.Write($"{i + 1}|");
+
+
+                    for (int j = 0; j < I; j++)
+                    {
+                        Console.Write($"   {distances[i, j]}");
+                    }
+                    Console.WriteLine($" |");
+                }
+            #endregion
+
+
+
+
+
+            int minValue = int.MaxValue;
+
+            int ff = depoNumber.Count();
+            Console.WriteLine(ff);
+            for (int i = 0; i < ff; i++)
+            {
+                for (int j = 0; j < I; j++)
+                {
+                    for(int k = 0; k < I; k++)
+                    {
+                        if (distances[depoNumber[i], k] < minValue && distances[depoNumber[i], k] != 0)
+                        {
+                            minValue = distances[depoNumber[i], k];
+                        }
+                    }
+                }
+               
+            }
+            Console.WriteLine();
+            Console.WriteLine($"minValue = {minValue}");
+            Console.WriteLine();
+
+            int maxValue = 0;
+            int optimal = 0;
+            for (int j = 0; j < ff; j++)
+            {
+                for (int i = 0; i < I; i++)
+                {
+                    // Проверяем, что перекресток i не является депо
+                    if (!depoNumber.Contains(i))
+                    {
+                        for (int k = 0; k < I; k++)
+                        {
+                            if (distances[i, k] > maxValue)
+                            {
+                                maxValue = distances[i, k];
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"maxValue 1 = {maxValue} ");
+
+            for (int i = 0; i < ff; i++)
+            {
+                for (int j = 0; j < I; j++)
+                {
+                    for (int k = 0; k < I; k++)
+                    {
+
+                        if (distances[depoNumber[i], k] == minValue && distances[depoNumber[i], k] != maxValue)
+                        {
+                            distances[depoNumber[i], k] = 0;
+                            distances[k, depoNumber[i]] = 0;
+                            for (int l = 0; l < I; l++)
+                            {
+                                distances[k, l] = -1;
+                                distances[l, k] = -1;
+
+                            }
+                        }
+
+
+                    }
+                }
+            }
+
+            maxValue = 0;
+            #region ShowArray
+            Console.WriteLine("    1    2    3    4    5    6");
+            Console.WriteLine("    ---------------------------");
+            for (int i = 0; i < I; i++)
+            {
+                Console.Write($"{i + 1}|");
+
+
+                for (int j = 0; j < I; j++)
+                {
+                    Console.Write($"   {distances[i, j]}");
+                }
+                Console.WriteLine($" |");
+            }
+            #endregion
+    
+            // Находим максимальное значение, исключая перекрестки, в которых уже есть депо
+
+            for (int j = 0; j < ff; j++)
+            {
+                for (int i = 0; i < I; i++)
+                {
+                    // Проверяем, что перекресток i не является депо
+                    if (!depoNumber.Contains(i))
+                    {
+                        for (int k = 0; k < I; k++)
+                        {
+                            if (distances[i, k] > maxValue )
+                            {
+                                maxValue = distances[i, k];
+                                optimal = i + 1; // Используем индекс i + 1, чтобы получить номер перекрестка
+                            }
+                        }
+                    }
+                }
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine($"maxValue 2 = {maxValue}");
+            Console.WriteLine();
+
+
+           Console.WriteLine(optimal);
         }
-
-        return (optimalIntersectionNumber, maxDistance);
-    }
-
-
-    public class Intersections
-    {
-        public int Number;
-        public int Neighbor;
-        public int Distance;
-        public bool hasDepo;
-
-       public Intersections(int Number, int Neighbor, int Distance) 
-        {
-            this.Number = Number;
-            this.Neighbor = Neighbor;
-            this.Distance = Distance;
-            this.hasDepo = false;
-        }
-
     }
 }
--- lab5/lab5/Program.cs
+++ lab5/lab5/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<(int, int, int)> FindMinSquares(int N)
+    {
+        int[] dp = new int[N + 1];
+        List<(int, int, int)> squares = new List<(int, int, int)>();
+
+        for (int i = 1; i <= N; i++)
+        {
+            dp[i] = i * i;
+            for (int j = 1; j < i; j++)
+            {
+                if (j * j > i)
+                    break;
+                dp[i] = Math.Min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+
+        while (N > 0)
+        {
+            int side = (int)Math.Sqrt(dp[N] - 1) + 1;
+            squares.Add((1, 1, side));
+            N -= side * side;
+        }
+
+        return squares;
+    }
+
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+        for (int t = 0; t < T; t++)
+        {
+            int N = int.Parse(Console.ReadLine());
+            List<(int, int, int)> result = FindMinSquares(N);
+            Console.WriteLine(result.Count);
+            foreach (var square in result)
+            {
+                Console.WriteLine($"{square.Item1} {square.Item2} {square.Item3}");
+            }
+        }
+    }
+}
